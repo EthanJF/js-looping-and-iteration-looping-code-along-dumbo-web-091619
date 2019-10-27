363c8c218a8eac9d3f6cd69b71ea88d7b654287d
EthanJF <ethanjosephfournier@gmail.com>
2019-10-27 18:47:06 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(arrayOfNames, eventName) {
+    const result = []
+    for (let i = 0; i < arrayOfNames.length; i++) {
+        result.push(`Thank you, ${arrayOfNames[i]}, for the wonderful surprise gift!`);
+    }
+    return result;
+}
+
+function countDown(number) {
+    while (number >= 0) {
+        console.log(number);
+        number--;
+    }
+}
